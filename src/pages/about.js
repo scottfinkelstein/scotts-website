d14c0497dfecd4ce4327e43d1da8f1cbeb108053
Scott Finkelstein <scottfinkelstein@users.noreply.github.com>
2021-03-14 17:31:59 -0400

    Added Clubhouse Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,6 +13,8 @@ class About extends React.Component {
                 <SEO title="About Me" />
                 <div className="container">
                     <p>I manage the Mobile and Web Technologies team at <a href="https://adelphi.edu" target="_blank" rel="noopener noreferrer">Adelphi University,</a> overseeing development for both the university website and mobile app. All posts are my own.</p>
+                    <h3>Follow Me On:</h3>
+                    <p>👋 <a href="https://www.joinclubhouse.com/@sbf0202" target="_blank">Clubhouse</a></p>
                 </div>
             </Layout>
         )
